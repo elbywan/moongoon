d48445b64c188c3314a0995660bbc983338e7bd1
elbywan <elbywan@hotmail.com>
2020-06-15 10:30:59 +0200

    upgrade mongo.cr dependency

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,9 +1,6 @@
 require "spec"
 require "../src/moongoon"
 
-backend = Log::IOBackend.new
-Log.builder.bind "*", :debug, backend
-
 ::Moongoon.after_connect_before_scripts {
   ::Moongoon.connection { |db|
     db.drop
